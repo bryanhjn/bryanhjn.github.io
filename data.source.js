622fd5ba34630d346a97b7d22fcdc361a652ada3
huangjn <huangjn@liandisys.com.cn>
2024-06-14 13:56:52 +0800

    test

---
+++ data.source.js
@@ -0,0 +1,357 @@
+import React from 'react';
+export const Banner00DataSource = {
+  wrapper: { className: 'banner0' },
+  textWrapper: { className: 'banner0-text-wrapper' },
+  title: {
+    className: 'banner0-title lxe8mqtor1-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <span>
+                <p>
+                  <span>中岛迷你储物盒</span>
+                </p>
+                <p>订单进度查询</p>
+              </span>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'banner0-content',
+    children: (
+      <span>
+        <span>
+          <p>由 初霜造物 设计、制造、组装</p>
+        </span>
+      </span>
+    ),
+  },
+  button: { className: 'banner0-button', children: 'Learn More' },
+};
+export const Feature80DataSource = {
+  wrapper: { className: 'home-page-wrapper feature8-wrapper' },
+  page: { className: 'home-page feature8' },
+  OverPack: { playScale: 0.3 },
+  titleWrapper: {
+    className: 'feature8-title-wrapper',
+    children: [
+      {
+        name: 'title',
+        className: 'feature8-title-h1',
+        children: (
+          <span>
+            <p>订单进度</p>
+          </span>
+        ),
+      },
+      {
+        name: 'content',
+        className: 'feature8-title-content',
+        children: (
+          <span>
+            <p>请根据闲鱼昵称查看你的订单进度</p>
+          </span>
+        ),
+      },
+    ],
+  },
+  childWrapper: {
+    className: 'feature8-button-wrapper',
+    children: [
+      {
+        name: 'button',
+        className: 'feature8-button',
+        children: { href: '#', children: '立即体验' },
+      },
+    ],
+  },
+  Carousel: {
+    dots: false,
+    className: 'feature8-carousel',
+    wrapper: { className: 'feature8-block-wrapper' },
+    children: {
+      className: 'feature8-block',
+      titleWrapper: {
+        className: 'feature8-carousel-title-wrapper',
+        title: { className: 'feature8-carousel-title' },
+      },
+      children: [
+        {
+          name: 'block0',
+          className: 'feature8-block-row',
+          gutter: 120,
+          title: {
+            className: 'feature8-carousel-title-block',
+            children: '平台自主训练流程',
+          },
+          children: [
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child0',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9a4go9f-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>原料采购</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children: (
+                      <span>
+                        <span>
+                          <span>
+                            <span>
+                              <p>
+                                U幼稚鬼（白PETG+紫玫喷漆）<br />
+                              </p>
+                              <p>
+                                Luther.卷（黑PETG）<br />
+                              </p>
+                              <p>
+                                tbNick_wkvt7（白PETG+紫玫喷漆）<br />
+                              </p>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
+                    ),
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child1',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9cwcrysc-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>等待排产</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child2',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9d061sai-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>已排产</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child3',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9d4iuaur-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <span>
+                          <p>等待运输</p>
+                        </span>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child~lxe906cw47k',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9d8v4uik-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <p>运输中</p>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                  },
+                ],
+              },
+            },
+            {
+              className: 'feature8-block-col',
+              md: 6,
+              xs: 24,
+              name: 'child~lxe9075vxi',
+              arrow: {
+                className: 'feature8-block-arrow',
+                children:
+                  'https://gw.alipayobjects.com/zos/basement_prod/167bee48-fbc0-436a-ba9e-c116b4044293.svg',
+              },
+              children: {
+                className: 'feature8-block-child lxe9ddp9sg7-editor_css',
+                children: [
+                  {
+                    name: 'image',
+                    className: 'feature8-block-image',
+                    children:
+                      'https://gw.alipayobjects.com/zos/basement_prod/d8933673-1463-438a-ac43-1a8f193ebf34.svg',
+                  },
+                  {
+                    name: 'title',
+                    className: 'feature8-block-title',
+                    children: (
+                      <span>
+                        <p>交付完成</p>
+                      </span>
+                    ),
+                  },
+                  {
+                    name: 'content',
+                    className: 'feature8-block-content',
+                    children:
+                      '沟通业务需求，对接人：诚凡、芸彩沟通业务需求，对接人：诚凡、芸彩',
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        <span>©2024 初霜造物 保留所有权利</span>
+      </span>
+    ),
+  },
+};
